943b7472764fdb288386c032084d3b1f12d42fb3
DvrkDvys <jharris@wework.com>
2018-07-20 16:45:35 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,7 @@
 class Student
 
-  attr_accessor :name
-
-  def initialize
-    @name = name
-  end
-
   def hello
-    puts "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
